b5d0d52aedfc5d52c4193e57e93b7fa64bb9573f
Rahul Kumar Mandal <rahul.mandal@algoscale.com>
2018-07-11 18:46:07 +0530

    done with database connection

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  "database": "",
+  "database": "mongodb://officialrahulmandal:12345Rahul@ds133601.mlab.com:33601/user_stories",
   "port": process.env.PORT || 3000,
   "secretKey": "YourSecretKey"
 }
--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require("./config");
+var mongoose = require('mongoose');
+var app = express();
 
 
 var app = express();
+
+mongoose.connect(config.database, function(err) {
+  if(err) {
+    console.log(err)
+  } else {
+    console.log('Connected to the database');
+  }
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
